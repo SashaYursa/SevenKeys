6140c3c2cadbfbce9b329b0d1575f2f9ba553647
SashaYursa <sasha.yursa13@gmail.com>
2023-09-05 19:20:33 +0300

    finally update before release

--- script.js
+++ script.js
@@ -31,3 +31,36 @@ function setActiveSlide(id){
 function removeActiveSlide(id){
     slides[id].classList.remove('active');
 }
+
+// burger
+
+document.querySelector('.header__burger').addEventListener('click', e=> {
+    e.preventDefault();
+    toggleBurger();
+})
+document.querySelectorAll('.header__nav-item').forEach(button => {
+    button.addEventListener('click',(e)=>{
+        toggleBurger();
+    })
+});
+
+const toggleBurger = () =>{
+    document.querySelector('.header__burger').classList.toggle('active');
+    document.querySelector('.header__nav').classList.toggle('active');
+    document.querySelector('body').classList.toggle('block');
+    document.querySelector('.pre-header').classList.toggle('active');
+}
+
+document.querySelectorAll('.request').forEach(element => {
+    element.addEventListener('click', e=>{
+        e.preventDefault();
+        document.querySelector('.modal').classList.toggle('open');
+        document.querySelector('body').classList.toggle('block');
+    })
+});
+document.querySelector('.modal__close').addEventListener('click', e=>{
+    e.preventDefault();
+    e.stopPropagation();
+    document.querySelector('.modal').classList.remove('open');
+    document.querySelector('body').classList.remove('block');
+})
